f48c87579f9dfb92c6302c8d9a1490ddd6833e29
Rodrigo A. Quezada Ccahuana <danter9462@gmail.com>
2025-10-28 18:12:05 -0500

    nuevos cambios 9.0

--- app/src/main/java/com/example/parkview/GoogleMaps.kt
+++ app/src/main/java/com/example/parkview/GoogleMaps.kt
@@ -13,6 +13,7 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -55,6 +56,10 @@ class GoogleMaps : Fragment(), OnMapReadyCallback {
         super.onViewCreated(view, savedInstanceState)
 
         val btnRegresar = view.findViewById<Button>(R.id.btn_regresar_maps)
+        btnRegresar.isSelected = true
+        btnRegresar.setOnClickListener {
+            findNavController().popBackStack()
+        }
 
         mapView = view.findViewById(R.id.mapView)
         mapView.onCreate(savedInstanceState)
